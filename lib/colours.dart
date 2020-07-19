ebc5424bf82756995b7a19e3311e23efb9e2972b
Hussein Abdallah <hussein.abdallah@ubanquity.com>
2020-07-19 16:43:51 +0300

    added cmyk and CIELAB constructors

--- lib/colours.dart
+++ lib/colours.dart
@@ -1,8 +1,12 @@
 library colours;
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 
 class Colours extends Color {
+  
+
   /// Construct a color from the lower 32 bits of an [int].
   ///
   /// The bits are interpreted as follows:
@@ -20,7 +24,7 @@ class Colours extends Color {
   /// Color(0xFFFF9000)` (`FF` for the alpha, `FF` for the red, `90` for the
   /// green, and `00` for the blue).
   @pragma('vm:entry-point')
-  const Colours(int value) : super(value);
+  Colours(int value) : super(value);
 
   /// Construct a color from the lower 8 bits of four integers.
   ///
@@ -34,7 +38,7 @@ class Colours extends Color {
   ///
   /// See also [fromRGBO], which takes the alpha value as a floating point
   /// value.
-  const Colours.fromARGB(int a, int r, int g, int b) : super.fromARGB(a,r,g,b);
+  Colours.fromARGB(int a, int r, int g, int b) : super.fromARGB(a,r,g,b);
 
   /// Create a color from red, green, blue, and opacity, similar to `rgba()` in CSS.
   ///
@@ -47,12 +51,88 @@ class Colours extends Color {
   /// Out of range values are brought into range using modulo 255.
   ///
   /// See also [fromARGB], which takes the opacity as an integer value.
-  const Colours.fromRGBO(int r, int g, int b, double opacity) : super.fromRGBO(r,g,b,opacity);
+  Colours.fromRGBO(int r, int g, int b, double opacity) : super.fromRGBO(r,g,b,opacity);
+
+  Colours.fromCIELAB(int l, int a, int b, int alpha) : super(_valfromCIE(l,a,b,alpha));
+  // from CIELAB Helpers
+  static int _valfromCIE(int l, int a, int b, int alpha) {
+        var Y = (l + 16.0)/116.0;
+        var X = a/500 + Y;
+        var Z = Y - b/200;
+        X = _deltaXYZ(X)*0.95047;
+        Y = _deltaXYZ(Y)*1.000;
+        Z = _deltaXYZ(Z)*1.08883;
+        var R = X*3.2406 + (Y * -1.5372) + (Z * -0.4986);
+        var G = (X * -0.9689) + Y*1.8758 + Z*0.0415;
+        var B = X*0.0557 + (Y * -0.2040) + Z*1.0570;
+        Color c = Color.fromARGB(alpha, _deltaRGB(R), _deltaRGB(G), _deltaRGB(B));
+        return c.value;
+  }
+
+  static _deltaXYZ(double k) {
+    return (pow(k, 3.0) > 0.008856) ? pow(k, 3.0) : (k - 4/29.0)/7.787;
+  }
+
+  static _deltaRGB(double k) {
+      return (k > 0.0031308) ? 1.055 * (pow(k, (1/2.4))) - 0.055 : k * 12.92;
+  }
+
+
+  Colours.fromCMYK(double c, double m, double y, double k) : super(_valfromCMYK(c,m,y,k));
+  // from CMYK Helpers
+
+  static int _valfromCMYK(double c, double m, double y, double k) {
+      var C = _cmyTransform(c, k);
+      var M = _cmyTransform(m, k);
+      var Y = _cmyTransform(y, k);
+      Color c = Color.fromARGB(1, 1 - C, 1 - M, 1 - Y);
+      return c.value;
+  }
+
+  static _cmyTransform(double x, double k) {
+      return x * (1 - k) + k;
+  }
 
   
-  
-
-
-  
+    
+    // func hue() -> CGFloat {
+    //     return hsba().h
+    // }
+    
+    // func saturation() -> CGFloat {
+    //     return hsba().s
+    // }
+    
+    // func brightness() -> CGFloat {
+    //     return hsba().b
+    // }
+    
+    // func CIE_Lightness() -> CGFloat {
+    //     return CIE_LAB().l
+    // }
+    
+    // func CIE_a() -> CGFloat {
+    //     return CIE_LAB().a
+    // }
+    
+    // func CIE_b() -> CGFloat {
+    //     return CIE_LAB().b
+    // }
+    
+    // func cyan() -> CGFloat {
+    //     return cmyk().c
+    // }
+    
+    // func magenta() -> CGFloat {
+    //     return cmyk().m
+    // }
+    
+    // func yellow() -> CGFloat {
+    //     return cmyk().y
+    // }
+    
+    // func keyBlack() -> CGFloat {
+    //     return cmyk().k
+    // }
   
 }
