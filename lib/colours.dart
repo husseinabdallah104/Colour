701bdf03278167cdd8ce0b590418aeddf4d54782
Hussein Abdallah <hussein.abdallah@ubanquity.com>
2020-07-21 12:31:06 +0300

    added missing cmyk cieLAB getters

--- lib/colours.dart
+++ lib/colours.dart
@@ -3,58 +3,38 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
+enum ColorScheme {
+  analagous, monochromatic, triad, complementary
+}
+    
+enum ColorFormulation {
+  rgba, hsba, lab, cmyk
+}
 
-class Colours extends Color {
+enum ColorDistance {
+  cie76, cie94, cie2000
+}
+
+enum ColorComparison {
+  darkness, lightness, desaturated, saturated, red, green, blue
+}
+
+class LMColor extends Color {
   
+  
+  LMColor(int value) : super(value);
 
-  /// Construct a color from the lower 32 bits of an [int].
-  ///
-  /// The bits are interpreted as follows:
-  ///
-  /// * Bits 24-31 are the alpha value.
-  /// * Bits 16-23 are the red value.
-  /// * Bits 8-15 are the green value.
-  /// * Bits 0-7 are the blue value.
-  ///
-  /// In other words, if AA is the alpha value in hex, RR the red value in hex,
-  /// GG the green value in hex, and BB the blue value in hex, a color can be
-  /// expressed as `const Color(0xAARRGGBB)`.
-  ///
-  /// For example, to get a fully opaque orange, you would use `const
-  /// Color(0xFFFF9000)` (`FF` for the alpha, `FF` for the red, `90` for the
-  /// green, and `00` for the blue).
-  @pragma('vm:entry-point')
-  Colours(int value) : super(value);
+  LMColor.fromARGB(int a, int r, int g, int b) : super.fromARGB(a,r,g,b);
 
-  /// Construct a color from the lower 8 bits of four integers.
-  ///
-  /// * `a` is the alpha value, with 0 being transparent and 255 being fully
-  ///   opaque.
-  /// * `r` is [red], from 0 to 255.
-  /// * `g` is [green], from 0 to 255.
-  /// * `b` is [blue], from 0 to 255.
-  ///
-  /// Out of range values are brought into range using modulo 255.
-  ///
-  /// See also [fromRGBO], which takes the alpha value as a floating point
-  /// value.
-  Colours.fromARGB(int a, int r, int g, int b) : super.fromARGB(a,r,g,b);
+  LMColor.fromRGBO(int r, int g, int b, double opacity) : super.fromRGBO(r,g,b,opacity);
 
-  /// Create a color from red, green, blue, and opacity, similar to `rgba()` in CSS.
-  ///
-  /// * `r` is [red], from 0 to 255.
-  /// * `g` is [green], from 0 to 255.
-  /// * `b` is [blue], from 0 to 255.
-  /// * `opacity` is alpha channel of this color as a double, with 0.0 being
-  ///   transparent and 1.0 being fully opaque.
-  ///
-  /// Out of range values are brought into range using modulo 255.
-  ///
-  /// See also [fromARGB], which takes the opacity as an integer value.
-  Colours.fromRGBO(int r, int g, int b, double opacity) : super.fromRGBO(r,g,b,opacity);
+  LMColor.fromCIELAB(int l, int a, int b, int alpha) : super(_valfromCIE(l,a,b,alpha));
 
-  Colours.fromCIELAB(int l, int a, int b, int alpha) : super(_valfromCIE(l,a,b,alpha));
+  LMColor.fromCMYK(double c, double m, double y, double k) : super(_valfromCMYK(c,m,y,k));
+
+
   // from CIELAB Helpers
+  
   static int _valfromCIE(int l, int a, int b, int alpha) {
         var Y = (l + 16.0)/116.0;
         var X = a/500 + Y;
@@ -78,47 +58,89 @@ class Colours extends Color {
   }
 
 
-  Colours.fromCMYK(double c, double m, double y, double k) : super(_valfromCMYK(c,m,y,k));
   // from CMYK Helpers
 
   static int _valfromCMYK(double c, double m, double y, double k) {
       var C = _cmyTransform(c, k);
       var M = _cmyTransform(m, k);
       var Y = _cmyTransform(y, k);
-      Color c = Color.fromARGB(1, 1 - C, 1 - M, 1 - Y);
-      return c.value;
+      Color color = Color.fromARGB(1, 1 - C, 1 - M, 1 - Y);
+      return color.value;
   }
 
   static _cmyTransform(double x, double k) {
       return x * (1 - k) + k;
   }
 
+  cieLAB() {
+        double r = red / 255.0;
+        double g = green / 255.0;
+        double b = blue / 255.0;
+
+        // Create deltaRGB
+        r = (r > 0.04045) ? pow((r + 0.055) / 1.055, 2.40) : (r / 12.92);
+        g = (g > 0.04045) ? pow((g + 0.055) / 1.055, 2.40) : (g / 12.92);
+        b = (b > 0.04045) ? pow((b + 0.055) / 1.055, 2.40) : (b / 12.92);
+
+        // Create XYZ
+        double X = r * 41.24 + g * 35.76 + b * 18.05;
+        double Y = r * 21.26 + g * 71.52 + b * 7.22;
+        double Z = r * 1.93 + g * 11.92 + b * 95.05;
+
+        // Convert XYZ to L*a*b*
+        X = X / 95.047;
+        Y = Y / 100.000;
+        Z = Z / 108.883;
+        X = (X > pow((6.0 / 29.0), 3.0)) ? pow(X, 1.0 / 3.0) : (1 / 3) * pow((29.0 / 6.0), 2.0) * X + 4 / 29.0;
+        Y = (Y > pow((6.0 / 29.0), 3.0)) ? pow(Y, 1.0 / 3.0) : (1 / 3) * pow((29.0 / 6.0), 2.0) * Y + 4 / 29.0;
+        Z = (Z > pow((6.0 / 29.0), 3.0)) ? pow(Z, 1.0 / 3.0) : (1 / 3) * pow((29.0 / 6.0), 2.0) * Z + 4 / 29.0;
+        double cieL = 116 * Y - 16;
+        double cieA = 500 * (X - Y);
+        double cieB = 200 * (Y - Z);
+        List<double> cieLAB = [cieL, cieA, cieB];
+        return cieLAB;
+    }
+
+    cmyk() {
+        // TODO: check rgb value correctness
+        double c = 1 - red / 255;
+        double m = 1 - green / 255;
+        double y = 1 - blue / 255;
+        double k = min(1, min(c, min(m, y)));
+        if (k == 1) {
+            c = 0;
+            m = 0;
+            y = 0;
+        } else {
+            c = (c - k) / (1 - k);
+            m = (m - k) / (1 - k);
+            y = (y - k) / (1 - k);
+        }
+        List<double> cmyk = [c, m, y, k];
+        return cmyk;
+    }
   
+  // double get hue => hsba()[0];
+
+  // double get saturation => hsba()[1];
+
+  // double get brightness => hsba()[2];
+
+  double get cieLightness => cieLAB()[0];
+
+  double get cieA => cieLAB()[1];
+
+  double get cieB => cieLAB()[2];
     
-    // func hue() -> CGFloat {
-    //     return hsba().h
-    // }
-    
-    // func saturation() -> CGFloat {
-    //     return hsba().s
-    // }
-    
-    // func brightness() -> CGFloat {
-    //     return hsba().b
-    // }
-    
-    // func CIE_Lightness() -> CGFloat {
-    //     return CIE_LAB().l
-    // }
-    
-    // func CIE_a() -> CGFloat {
-    //     return CIE_LAB().a
-    // }
-    
-    // func CIE_b() -> CGFloat {
-    //     return CIE_LAB().b
-    // }
-    
+  
+  double get cyan => cmyk()[0];
+
+  double get magenta => cmyk()[1];
+
+  double get yellow => cmyk()[2];
+
+  double get keyBlack => cmyk()[3];
+
     // func cyan() -> CGFloat {
     //     return cmyk().c
     // }
